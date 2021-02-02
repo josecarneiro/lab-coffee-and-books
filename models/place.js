52554ecccb5fdb41379b975637a63b78f1cc7240
Dardannya Patente <dardannyapatente@hotmail.com>
2021-02-02 14:21:14 +0100

    changes

---
+++ models/place.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const schema = new mongoose.Schema({
+  name: {
+    type: String,
+  },
+//   location: {
+//     coordinates: [
+//       {
+//         type: Number,
+//         min: -180,
+//         max: 180
+//       }
+//     ],
+    type: {
+      type: String,
+      enum: ['coffee_shop', 'bookstore'] 
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+const Place = mongoose.model('Place', schema);
+
+module.exports = Place;
+
+
